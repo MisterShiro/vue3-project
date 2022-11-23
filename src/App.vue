ebb0f0205cb781b740f8b2e933542fdcbb5e4489
shiro <1721192262@qq.com>
2022-11-23 18:29:52 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,70 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <p>ref:{{ name }}</p>
+  <p>reactive:{{ person.name }}</p>
+  <p>computed:{{ plusOne }}</p>
+  <button @click="setValut">操作1</button>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<script lang="ts" setup>
+import { computed, watch } from 'vue';
+import {
+  onBeforeMount,
+  onBeforeUnmount,
+  onBeforeUpdate,
+  onMounted,
+  onUnmounted,
+  onUpdated,
+  reactive,
+  ref,
+} from 'vue';
+// ref
+const name = ref<String>('张三');
+// reactive
+interface Person {
+  name: string;
+  age: number;
 }
+const person = reactive<Person>({
+  name: '张三',
+  age: 18,
+});
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+// computed
+const plusOne = computed(() => {
+  let data: any = '';
+  data = person.age;
+  return data;
+});
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+// watch(在监听reactive属性时 需要以()=>{}的形式去监听)
+watch(
+  () => person.name,
+  (newValue, oldValue) => {
+    console.log(newValue, oldValue);
   }
+);
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+function setValut() {
+  person.name = '王五';
+  name.value = '王五';
 }
-</style>
+
+onBeforeMount(() => {
+  console.log('onBeforeMount================>');
+});
+onMounted(() => {
+  console.log('onMounted================>');
+});
+onBeforeUpdate(() => {
+  console.log('onBeforeUpdate================>');
+});
+onUpdated(() => {
+  console.log('onUpdated================>');
+});
+onBeforeUnmount(() => {
+  console.log('onBeforeUnmount================>');
+});
+onUnmounted(() => {
+  console.log('onUnmounted================>');
+});
+</script>
+<style lang="scss" scoped></style>
